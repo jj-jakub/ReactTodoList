fea26b981fa5400d47fa6f0516641244426e48fd
jj <>
2021-05-27 18:24:53 +0200

    Import js files

---
+++ src/constants/Constants.js
@@ -0,0 +1,10 @@
+const Constants = {
+    allItemsTabName: 'All items',
+    finishedItemsTabName: 'Finished items',
+    todoItemsTabName: 'TODO',
+    serverAddress: 'http://localhost:4000',
+    getTodosEndpoint: '/api/todos',
+    deleteItemEndpoint: '/api/todos/'
+}
+
+export default Constants
